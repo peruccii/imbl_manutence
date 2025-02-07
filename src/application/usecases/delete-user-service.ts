c137baa9152b7bdc0688f0f9923f846b7f58e34a
Eduardo Perucci Martins de Souza <eduardo.perucci@sp.senai.br>
2025-02-07 12:43:37 -0300

    delete user

--- src/application/usecases/delete-user-service.ts
+++ src/application/usecases/delete-user-service.ts
@@ -4,7 +4,6 @@ export class DeleteUserService {
     constructor(private readonly userRepository: UserRepository) {}
 
     async execute(id: string) {
-
         const userExists = this.userRepository.findOne(id)
 
         if (!userExists) throw new Error("user not found")
