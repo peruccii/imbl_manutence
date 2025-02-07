76cf4344ecf01f81a8c604181cc496c416edfd4c
Eduardo Perucci Martins de Souza <eduardo.perucci@sp.senai.br>
2025-02-07 13:16:06 -0300

    auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,22 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Dependencies, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UserRepository } from 'src/application/repositories/user-repository';
 
+@Dependencies(JwtService)
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UserRepository) {}
+    constructor(private usersService: UserRepository, private jwtService) {
+      this.jwtService = jwtService;
+    }
 
     async signIn(username: string, pass: string): Promise<any> {
       const user = await this.usersService.findOne(username);
       if (user?.password !== pass) {
         throw new UnauthorizedException();
       }
-      const { password, ...result } = user;
-      return result;
+      const payload = { username: user.name, sub: user.id };
+      return {
+        access_token: await this.jwtService.signAsync(payload),
+      };
     }
 }
