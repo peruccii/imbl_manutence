1560e57ae9268a43a737d1e8f6ca446f0ec30552
Eduardo Perucci Martins de Souza <eduardo.perucci@sp.senai.br>
2025-02-07 13:55:00 -0300

    manutence logic created // todo: refatorate

--- src/application/usecases/user-create-service.ts
+++ src/application/usecases/user-create-service.ts
@@ -2,16 +2,22 @@ import { Injectable } from "@nestjs/common";
 import { makeUserFactory } from "../factories/user-factory";
 import { CreateUserRequest } from "../interfaces/user-create-request";
 import { PrismaUserMapper } from "src/infra/database/prisma/mappers/prisma-user-mapper";
+import { UserRepository } from "../repositories/user-repository";
 
 @Injectable()
 export class UserCreateService {
-    constructor() {}
+    constructor(private readonly userRepository: UserRepository) {}
 
     async execute(request: CreateUserRequest) {
+
+        const userExists = await this.userRepository.findOne(request.email)
+
+        if (userExists) throw new Error("user already exists")
+
         const user = makeUserFactory({
             ...request
         })
-        
+
         PrismaUserMapper.toPrisma(user)
     }
 }
