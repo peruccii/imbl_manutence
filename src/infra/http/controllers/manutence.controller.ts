60607c39b662d6e116c18a27b60da820ccc1cbbd
Eduardo Perucci Martins de Souza <eduardo.perucci@sp.senai.br>
2025-02-10 10:29:44 -0300

    todo: <use> multer file

--- src/infra/http/controllers/manutence.controller.ts
+++ src/infra/http/controllers/manutence.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { Role } from 'src/application/enums/role.enum';
+import { CreateManutenceRequest, ManutenceCreateService } from 'src/application/usecases/manutence-create-service';
+import { AuthGuard } from 'src/auth/auth.guard';
 import { Roles } from 'src/roles/roles.decorator';
 
 @Controller()
 export class ManutenceController {
-  constructor() {}
+  constructor(private readonly manutence_service: ManutenceCreateService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
-  @Roles(Role.ADMIN)
-  async createManutence() {}
+  @Roles(Role.DEFAULT)
+  async createManutence(@Body() request: CreateManutenceRequest) {
+    return await this.manutence_service.execute(request)
+  }
 }
